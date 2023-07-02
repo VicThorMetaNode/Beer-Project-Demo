a21bd54f81f7c682032e9825247b6b37332fdaff
unknown <git@github.com:VicThorMetaNode>
2023-07-02 12:54:04 +0200

    mobile size fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import WebgiViewer from "./components/WebgiViewer";
 function App() {
   return (
     <>
-      <section className="flex justify-center items-center mt-20">
-        <div className="mx-auto">
-          <p className="font-main font-bold text-alt-white text-5xl">
+      <section className="flex justify-center items-center md:mt-20 h-screen md:h-auto">
+        <div className="px-4">
+          <p className="font-main font-bold text-alt-white text-3xl md:text-5xl text-center">
             BEER SHOP DEMO
           </p>
-          <div className="w-full mt-20 flex justify-center items-center">
+          <div className="w-full md:mt-20 flex justify-center items-center">
             <WebgiViewer className="w-full h-full" />
           </div>
         </div>
